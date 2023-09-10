fb89b04b39cf43140152b49aa9d3d99ae9faa8ed
BrittineyWashington <https://github.com>
2023-09-10 13:00:21 -0400

    5th push updated README and index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-// Coding Steps:
-// Using any of the tools you've worked with so far, create a game of Tic-Tac-Toe.
-// Create a Tic-Tac-Toe game grid using your HTML element of choice.
-// When a cell in the grid is clicked, an X or O should appear in that spot depending on whose turn it is.
-// A heading should say whether it is X's or O's turn and change with each move made.
-// A button should be available to clear the grid and restart the game.
-// When a player has won, or the board is full and the game results in a draw, a Bootstrap alert or similar Bootstrap component should appear across the screen announcing the winner.
-
+//Coding Steps: 
 //what I want to add to this is a way to keep score and show that at the bottom as a continual addition each time 
 //also refactor using bootstrap and jQuery
 //add a way to let the user enter their own name as a prompt and the option to play with the computer or another player. 
